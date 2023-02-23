affc50ec9631b9bdd9d14b0d182b70ea35f4461a
Shyamanth Kashyap <shyamanth.giggs@gmail.com>
2023-02-23 11:10:37 +0530

    Added Admin Bean, Admin Login API

---
+++ src/main/java/com/example/backend/Controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.example.backend.Controller;
+
+import com.example.backend.Bean.Admin;
+import com.example.backend.Service.AdminService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+import static javax.security.auth.callback.ConfirmationCallback.OK;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+    @Resource(name = "adminService")
+    private AdminService adminService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity addDoctor(@RequestBody Admin admin) {
+        Boolean status = adminService.login(admin);
+        if (status == Boolean.FALSE)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO doctor VALUES (1,'20120618',"123","123","123","AAA","A","B",true,"12
 INSERT INTO doctor VALUES (2,'20120618',"123","123","123","BBB","A","B",true,"123","123",5,'M',"Heart","123");
 INSERT INTO doctor VALUES (3,'20120618',"123","123","123","CCC","A","B",true,"123","123",5,'M',"Kidney","123");
 INSERT INTO doctor VALUES (4,'20120618',"123","123","123","DDD","A","B",true,"123","123",5,'M',"Kidney","123");
+INSERT INTO admin VALUES (1,"pwd", "user");
