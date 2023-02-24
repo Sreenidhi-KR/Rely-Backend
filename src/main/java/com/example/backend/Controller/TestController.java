8416354cc5aac46a09ce4fcc47930bdb7804944f
Shyamanth Kashyap <shyamanth.giggs@gmail.com>
2023-02-25 00:44:28 +0530

    Cross Origin Annotation

--- src/main/java/com/example/backend/Controller/TestController.java
+++ src/main/java/com/example/backend/Controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.backend.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/test")
 public class TestController {
 
