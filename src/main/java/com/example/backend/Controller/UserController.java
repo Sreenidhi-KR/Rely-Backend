67d15aa40af7133d0dd23b34b122f009a9daef82
Aditya M <aditya_gowda_2000@yahoo.com>
2023-04-04 23:24:25 +0530

    patinet profile create and get all profiles API's added

--- src/main/java/com/example/backend/Controller/UserController.java
+++ src/main/java/com/example/backend/Controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 
 @RestController
@@ -14,10 +15,15 @@ import javax.annotation.Resource;
 public class UserController {
     @Resource(name = "userService")
     private UserService userService;
-//
-//    @RequestMapping(value = "/addPatient", method = RequestMethod.POST)
-//    public ResponseEntity addPatient(@RequestBody Patient patient) {
-//        Boolean withinLimit = userService.addPatient(patient);
-//        return null;
-//    }
+
+    @RequestMapping(value="/addProfile/{userId}", method=RequestMethod.POST)
+    public void addPatient(@PathVariable int userId,@RequestBody Patient patient){
+        userService.addPatient(patient,userId);
+    }
+
+    @RequestMapping(value="/getProfiles/{userId}",method=RequestMethod.GET)
+    public List<Patient> getPatients(@PathVariable int userId){
+        List<Patient> patients=userService.getAllProfiles(userId);
+        return patients;
+    }
 }
--- src/main/java/com/example/backend/Service/UserService.java
+++ src/main/java/com/example/backend/Service/UserService.java
@@ -1,13 +1,49 @@
 package com.example.backend.Service;
 
 import com.example.backend.Bean.Patient;
+import com.example.backend.Bean.Role;
+import com.example.backend.Bean.User;
+import com.example.backend.Repository.PatientRepository;
+import com.example.backend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserService {
-//
-//    @Autowired
-//    private UserRepository userRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public List<Patient> getAllProfiles(int userId){
+        User user = userRepository.findUserById(userId);
+        System.out.println("working file till step 1");
+        List<Patient> patients = patientRepository.getPatients(userId);
+        System.out.println("working fine till step 2");
+        return patients;
+    }
+
+    public void addPatient(Patient patient, int userId){
+        //get user with the given id
+        User user=userRepository.findUserById(userId);
+        //add the new patient profile to the list of profiles and update
+        List<Patient>profiles = user.getProfiles();
+        if (profiles==null){
+            profiles=new ArrayList<>();
+        }
+        patient.setUser(user);
+        profiles.add(patient);
+        user.setProfiles(profiles);
+        userRepository.save(user);
+    }
+
+    public void removePatient(int patientId){
+        patientRepository.deletePatientById(patientId);
+    }
 
 }
